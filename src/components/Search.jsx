2514a577b897d1e7b5ebdbf9a0af4992a9dd37b5
bogdan <bogdanbogdanvakulenka@gmail.com>
2025-04-26 17:47:31 +0300

    search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "../styles/search.module.css";
+const Search = () => {
+  return (
+    <form className={styles.search}>
+      {/* icon */}
+      <div className={styles.search__icon}></div>
+      {/* input field */}
+      <input
+        className={styles.search__field}
+        placeholder="search..."
+        type="text"
+      />
+      {/* submit button */}
+      <button type="button" className={styles.search__submit}>
+        <img src="/search_icon.svg" alt="search" />
+      </button>
+    </form>
+  );
+};
+
+export default Search;
