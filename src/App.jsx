f0806066fec28dbf65916ae18ec3d4e122ff3948
Anton Boicheniuk <antonboixenyk2002@gmail.com>
2025-04-26 21:36:10 +0300

    Basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import MoviePage from './pages/MoviePage';
-import FavoritesPage from './pages/FavoritesPage';
-import SessionsPage from './pages/SessionsPage';
-import AdminPanel from './pages/AdminPanel';
-import SearchPage from './pages/SearchPage';
-import NotFoundPage from './pages/NotFoundPage';
+//import MoviePage from './pages/MoviePage';
+//import FavoritesPage from './pages/FavoritesPage';
+//import SessionsPage from './pages/SessionsPage';
+//import AdminPanel from './pages/AdminPanel';
+//import SearchPage from './pages/SearchPage';
+//import NotFoundPage from './pages/NotFoundPage';
 
 import './styles/App.css';
 
@@ -17,22 +17,22 @@ function App() {
         <Route path="/" element={<HomePage />} />
 
         {/* Сторінка одного фільму */}
-        <Route path="/movie/:id" element={<MoviePage />} />
+       {/* <Route path="/movie/:id" element={<MoviePage />} />*/}
 
         {/* Сторінка обраного */}
-        <Route path="/favorites" element={<FavoritesPage />} />
+       {/* <Route path="/favorites" element={<FavoritesPage />} />*/}
 
         {/* Сторінка сеансів */}
-        <Route path="/sessions" element={<SessionsPage />} />
+       {/* <Route path="/sessions" element={<SessionsPage />} />*/}
 
         {/* Сторінка пошуку */}
-        <Route path="/search" element={<SearchPage />} />
+       {/* <Route path="/search" element={<SearchPage />} />*/}
 
         {/* Адмін панель */}
-        <Route path="/admin" element={<AdminPanel />} />
+        {/*<Route path="/admin" element={<AdminPanel />} />*/}
 
         {/* 404 сторінка */}
-        <Route path="*" element={<NotFoundPage />} />
+         {/*<Route path="*" element={<NotFoundPage />} />*/}
       </Routes>
     </Router>
   );
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+// src/pages/Home.jsx
+import styles from '../styles/Home.module.css';
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1>ЗАРАЗ У КІНО</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
