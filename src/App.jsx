4e49bc981b9ffa0b1a75d943fd4e863ecfd4693a
bogdan <bogdanbogdanvakulenka@gmail.com>
2025-04-26 22:06:39 +0300

    added header, wrapper to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,48 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import MoviePage from './pages/MoviePage';
-import FavoritesPage from './pages/FavoritesPage';
-import SessionsPage from './pages/SessionsPage';
-import AdminPanel from './pages/AdminPanel';
-import SearchPage from './pages/SearchPage';
-import NotFoundPage from './pages/NotFoundPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+// import MoviePage from "./pages/MoviePage";
+// import FavoritesPage from "./pages/FavoritesPage";
+// import SessionsPage from "./pages/SessionsPage";
+// import AdminPanel from "./pages/AdminPanel";
+// import SearchPage from "./pages/SearchPage";
+// import NotFoundPage from "./pages/NotFoundPage";
 
-import './styles/App.css';
+import "./styles/App.css";
+
+import Header from './components/Header';
+import Wrapper from './components/Wrapper';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Головна сторінка */}
-        <Route path="/" element={<HomePage />} />
-
-        {/* Сторінка одного фільму */}
-        <Route path="/movie/:id" element={<MoviePage />} />
-
-        {/* Сторінка обраного */}
-        <Route path="/favorites" element={<FavoritesPage />} />
-
-        {/* Сторінка сеансів */}
-        <Route path="/sessions" element={<SessionsPage />} />
-
-        {/* Сторінка пошуку */}
-        <Route path="/search" element={<SearchPage />} />
-
-        {/* Адмін панель */}
-        <Route path="/admin" element={<AdminPanel />} />
-
-        {/* 404 сторінка */}
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </Router>
+    <Wrapper>
+      <Header />
+      <Router>
+        <Routes>
+          {/* Головна сторінка */}
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </Wrapper>
   );
 }
+
+// {/* Сторінка одного фільму */}
+// <Route path="/movie/:id" element={<MoviePage />} />
+
+// {/* Сторінка обраного */}
+// <Route path="/favorites" element={<FavoritesPage />} />
+
+// {/* Сторінка сеансів */}
+// <Route path="/sessions" element={<SessionsPage />} />
+
+// {/* Сторінка пошуку */}
+// <Route path="/search" element={<SearchPage />} />
+
+// {/* Адмін панель */}
+// <Route path="/admin" element={<AdminPanel />} />
+
+// {/* 404 сторінка */}
+// <Route path="*" element={<NotFoundPage />} />
+
 
 export default App;
