a37b6cd47461c2c188a4d094dc14c806000ed243
bogdan <bogdanbogdanvakulenka@gmail.com>
2025-04-28 00:49:59 +0300

    Fix CSS path and add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import styles from "../styles/header.module.css";
+import styles from "../../styles/header.module.css";
 import IsSigned from "./IsSigned";
 import Search from "./Search";
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.logo}>logo</div>
+      <div className={styles.logo}>
+        <img src="/logo.svg" alt="logo" />
+      </div>
 
       <ul className={styles.list}>
         <li className={styles.list__item}>
