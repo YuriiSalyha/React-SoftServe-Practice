316279a08695223f9606e2abf0d35ad2740c945b
bogdan <bogdanbogdanvakulenka@gmail.com>
2025-04-29 02:50:28 +0300

    login logic

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import styles from '../styles/auth.module.css';
+import styles from "../styles/auth.module.css";
+import useAuth from "../hooks/useAuth";
+
 const signInSchema = z.object({
   login: z
     .string()
@@ -10,26 +12,54 @@ const signInSchema = z.object({
     .nonempty("Login is required"),
   password: z
     .string()
-    .min(6, "Password must be at least 6 characters")
+    .min(3, "Password must be at least 6 characters")
     .nonempty("Password is required"),
 });
 
 const SignInPage = () => {
+  const { login } = useAuth();
+
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors },
   } = useForm({
     resolver: zodResolver(signInSchema),
   });
 
-  const onSubmit = (data) => {
-    console.log("Login Data:", data);
+  const onSubmit = async (data) => {
+    try {
+      const response = await login({
+        username: data.login,
+        password: data.password,
+      });
+
+      if (!response.success) {
+        setError("login", {
+          type: "manual",
+          message: response?.error,
+        });
+        return;
+      }
+
+      alert("success!");
+    } catch (error) {
+      console.error(error);
+      setError("general", {
+        type: "manual",
+        message: "Сталася помилка. Спробуйте ще раз пізніше.",
+      });
+    }
   };
 
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.title}>Вхід</h1>
+
+      {errors.general && (
+        <p className={styles.field__error}>{errors.general.message}</p>
+      )}
 
       <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.field}>
@@ -38,11 +68,10 @@ const SignInPage = () => {
             className={styles.field__input}
             type="text"
             placeholder="Ваш логін"
-
             {...register("login")}
           />
-          {errors.email && (
-            <p className={styles.field__error}>{errors.email.message}</p>
+          {errors.login && (
+            <p className={styles.field__error}>{errors.login.message}</p>
           )}
         </div>
 
